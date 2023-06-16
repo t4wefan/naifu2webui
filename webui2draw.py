ee4e756fdb35c11aa34cb806a4b9d5f9bc90d907
t4wefan <66654403+t4wefan@users.noreply.github.com>
2023-06-17 00:05:09 +0800

    Update webui2draw.py

--- webui2draw.py
+++ webui2draw.py
@@ -7,7 +7,7 @@ from config import config
 
 from bs4 import BeautifulSoup
 
-def get_html_from_url(url):
+def get_text_from_url(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     html = soup.prettify()
